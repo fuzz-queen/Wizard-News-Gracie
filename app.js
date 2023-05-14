7ba950308f59d323cd929cd09ff5cda5a3965edf
Gracie <graciengainey@gmail.com>
2023-05-14 10:50:39 -0400

    pushing it all up

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 const express = require("express");
+const morgan = requre("morgan");
+const postBank = requre("./postBank")
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News by Gracie</title>
+    </head>
+    <body>
+      <ul>
+        ${postMessage.map(post => `<li>${}</li>`)} 
+      </ul>
+    </body>
+  </html><!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News by Gracie</title>
+    </head>
+    <body>
+      <ul>
+        ${postMessage.map(post => `<li>${}</li>`)} 
+      </ul>
+    </body>
+  </html>`
+
+  res.send(html)
+});
 
 const PORT = 1337;
 
